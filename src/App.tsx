c8d77e9d7c7ef0e74fa87ee0867acbf58290fee5
황규영 <arqhive@hwang-gyuyeong-ui-MacBookPro.local>
2021-10-18 13:53:25 +0900

    recoil 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React, { ReactElement } from 'react';
-import MyComponent from './react/Ref/Ref';
 
-// JSX는 ReactElement를 반환한다.
-// JSX도 expression 이다. 정규 함수 호출이 되고 객체로 인식된다.
 
 function App(): ReactElement {
   return (
     <div>
-      <MyComponent />
+      <h1>Hello</h1>
     </div>
   );
 }
